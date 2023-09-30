d231c6e794eaacb53cd1ce9297d0fba4d313811e
Amr Yasser Saber <amr.yassr.2@gmail.com>
2023-10-01 01:16:02 +0300

    Boilerplate

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+
+
+const Avatar = ({ img, alt }) => {
+    return (
+        <div className="rounded-full overflow-hidden w-full pt-[100%] relative">
+            <div className="absolute inset-0">
+                <img src={img} alt={alt || img} />
+            </div>
+        </div>
+    );
+
+}
+
+export default Avatar;
